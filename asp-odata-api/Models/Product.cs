865ea9e7633b705416937eb6bb7af918ff3f2d70
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-07 11:39:02 +0300

    Add project files.

---
+++ asp-odata-api/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_odata_api.Models
+{
+    public class Product
+    {
+    }
+}
---
+++ asp-odata-api/Services/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_odata_api.Services
+{
+    public class AppContext
+    {
+    }
+}
