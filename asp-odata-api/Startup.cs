43d1e80216ad328af557f597876bdbbdf9767aa6
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-07 12:51:11 +0300

    Update Startup.cs

--- asp-odata-api/Startup.cs
+++ asp-odata-api/Startup.cs
@@ -1,11 +1,17 @@
+using asp_odata_api.Models;
+using asp_odata_api.Services;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OData.Edm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +31,9 @@ namespace asp_odata_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(op => op.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
             services.AddControllers();
+            services.AddOData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,9 +50,16 @@ namespace asp_odata_api
 
             app.UseAuthorization();
 
+            #region Odata
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            builder.EntitySet<Category>("Categories");
+            #endregion
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.Select().Expand().Filter().OrderBy().MaxTop(50).Count();
+                endpoints.MapODataRoute("odata", "odata", builder.GetEdmModel());
             });
         }
     }
