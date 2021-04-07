5c613d5d0b9bb1530dcbf98f205e61e2efd4db66
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-07 12:51:35 +0300

    Update ProductsController

--- asp-odata-api/Controllers/ProductsController.cs
+++ asp-odata-api/Controllers/ProductsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using asp_odata_api.Models;
+using asp_odata_api.Services;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -7,10 +12,28 @@ using System.Threading.Tasks;
 
 namespace asp_odata_api.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ProductsController : ControllerBase
+
+    public class ProductsController : ODataController
     {
-       
+        private readonly ApplicationContext _context;
+
+        public ProductsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+
+        [EnableQuery]
+        public ActionResult Get()
+        {
+            return Ok(_context.Products.ToList());
+        }
+
+        [EnableQuery]
+        public ActionResult Get(int key)
+        {
+            return Ok(_context.Products.Find(key));
+        }
+
     }
 }
