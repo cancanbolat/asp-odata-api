08e9cedce91f7a7d035ba5ea20b4bd359a923806
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-08 11:44:18 +0300

    Add Customer model class

---
+++ asp-odata-api/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_odata_api.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
