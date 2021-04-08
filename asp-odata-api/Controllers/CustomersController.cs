36c2e60b3123193aece6847fb63a5b51f360c59b
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-04-08 11:44:35 +0300

    Add CustomersController. Using Sql Kata

---
+++ asp-odata-api/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_odata_api.Controllers
+{
+    
+    public class CustomersController : ODataController
+    {
+        private readonly QueryFactory _queryFactory;
+        private readonly ILogger<CustomersController> _logger;
+        
+        public CustomersController(QueryFactory queryFactory, ILogger<CustomersController> logger)
+        {
+            _queryFactory = queryFactory;
+            _logger = logger;
+        }
+
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            var result = _queryFactory.Query("Customers").Select("Name", "Country", "City").Get();
+            return Ok(result);
+        }
+
+        [EnableQuery]
+        public IActionResult Get(int key)
+        {
+            var result = _queryFactory.Query("Customers").Where("Id", key).Get();
+            return Ok(result);
+        }
+    }
+}
